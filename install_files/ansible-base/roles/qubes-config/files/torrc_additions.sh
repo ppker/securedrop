3629999ee989cee2a610ef4766eb2e92636e5c3e
Micah Lee <micah@micahflee.com>
2025-06-12 11:16:38 -0700

    Make qubes script torrc if it doesn't exist

--- install_files/ansible-base/roles/qubes-config/files/torrc_additions.sh
+++ install_files/ansible-base/roles/qubes-config/files/torrc_additions.sh
@@ -4,9 +4,20 @@ set -e
 LINE='ClientOnionAuthDir /rw/usrlocal/lib/tor/onion_auth'
 TORRC='/etc/tor/torrc'
 
-if grep -Fxq "$LINE" "$TORRC"; then
-    exit 0
-else
-    echo "$LINE" >> "$TORRC"
-    systemctl restart tor
+# Create /etc/tor directory if it doesn't exist
+if [ ! -d "$(dirname "$TORRC")" ]; then
+    mkdir -p "$(dirname "$TORRC")"
 fi
+
+# Create torrc file if it doesn't exist
+if [ ! -f "$TORRC" ]; then
+    touch "$TORRC"
+fi
+
+# Add the line if not present
+if ! grep -Fxq "$LINE" "$TORRC"; then
+    echo "$LINE" >> "$TORRC"
+fi
+
+# Restart tor
+systemctl restart tor
